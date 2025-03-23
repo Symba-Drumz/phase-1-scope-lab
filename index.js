9beb37d580c675e6efd3d423c33ce3dde7dd7c08
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-03-23 18:02:07 +0300

    added file: .results.json, modified file: index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'someone annoying';
+
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'someone else';
+}
